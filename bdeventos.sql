68acd3c006290a7ee8b7517ce42cffb3284b93a0
ananarcisio <125074969+ananarcisio@users.noreply.github.com>
2024-08-02 18:08:46 -0300

    Add files via upload

--- bdeventos.sql
+++ bdeventos.sql
@@ -20,3 +20,27 @@ CREATE TABLE IF NOT EXISTS fornecedores (
     estado VARCHAR(50) NOT NULL,
     setor VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(25) NOT NULL,
+    senha VARCHAR(12) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS empresa (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    razaosocial VARCHAR(100) NOT NULL,
+	nomefantasia VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(25) NOT NULL,
+    cnpj VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    complemento VARCHAR(100),
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    setor VARCHAR(20) NOT NULL
+)engine = InnoDb;
